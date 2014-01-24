63fe8ffe3c0ee342ec48357686643f46075e800f
Kyle Doherty <kyledoherty22@gmail.com>
2014-01-24 14:54:06 +0100

    added log and events page

--- logger.rb
+++ logger.rb
@@ -2,3 +2,16 @@ require 'sinatra'
 get '/' do 
 	"Box Event Logger"
 end 
+
+
+get '/log/*' do
+	params[:splat]
+	File.open("events.txt", "w"){|event| event.puts params[:splat]}
+end 
+
+post '/events' do 
+	log = File.open("events.txt", "r"){|event| event.read}
+	"#{log}"
+end
+	
+	
